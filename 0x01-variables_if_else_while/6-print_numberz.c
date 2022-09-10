a867eb7dcef0f5527cc7d8babe67b2583aa479ab
Michael <okoronumichael9224@gmail.com>
2022-09-10 07:50:29 -0700

    Task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+	num = 0;
+
+	while (num <= 9)
+	{
+		/*convert num to ASCII representation*/
+		putchar(num + '0');
+		++num;
+	}
+	putchat('\n');
+
+	return (0);
+}
