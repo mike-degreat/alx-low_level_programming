0d4acb2a54e984772fc10460df90db8b13286ff2
Michael <okoronumichael9224@gmail.com>
2022-09-10 12:55:53 -0700

    Task_8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 48; /*48; decimal rep of 0*/
+
+	while (i <= 102) /*102; decimal rep of f*/
+	{
+		putchar(i);
+
+		/*after 9 jump till 96;*/
+		if (i == 57)
+			i += 39;
+		++i;
+	}
+	putchar('\n');
+
+	return (0);
+}
