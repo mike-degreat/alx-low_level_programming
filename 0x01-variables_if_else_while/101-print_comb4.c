fb957df47cf0a16bc817b6dc254002749ad65d93
Michael <okoronumichael9224@gmail.com>
2022-09-10 17:57:19 -0700

    Task_11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	int num1, num2, num3 = 0;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
+				{
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				++num3;
+			}
+			++num2;
+		}
+		++num1;
+	}
+	putchar('\n');
+
+	return (0);
+}
