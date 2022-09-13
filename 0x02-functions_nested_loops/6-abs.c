e6cdfcf6846c3409cdd76894f030f45a20da2e6b
Michael <okoronumichael9224@gmail.com>
2022-09-13 14:32:47 -0700

    Task_6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that declares the absolute integer value
+ *
+ * @int - takes in integer type for input function
+ *
+ * Return: Always 0 (success)
+*/
+int _abs(int y)
+{
+	if (y < 0)
+	{
+		y *= (-1);
+		return (y);
+	}
+}
+
