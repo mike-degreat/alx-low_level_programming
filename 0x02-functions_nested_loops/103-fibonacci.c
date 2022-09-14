21e8ffcb0b5b4432d9d2ad8e4d85840148fa4108
Michael <okoronumichael9224@gmail.com>
2022-09-13 19:14:38 -0700

    Task_15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - prints the sum of all even fibonacci numbers below 4,000,000
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	long long int total_sum, sum, fib_num1, fib_num2;
+
+	total_sum = 0;
+	sum = 0;
+	fib_num1 = 0;
+	fib_num2 = 1;
+
+	while (sum < 4000000)
+	{
+		sum = fib_num1 + fib_num2;
+		if (sum % 2 == 0)
+		{
+			total_sum += sum;
+		}
+		fib_num1 = fib_num2;
+		fib_num2 = sum;
+	}
+	printf("%li\n", total_sum);
+
+	return (0);
+}
