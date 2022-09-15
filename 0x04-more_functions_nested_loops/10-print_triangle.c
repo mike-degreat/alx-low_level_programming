d2fc166b5a2ca880e5a7e316f10c43ffbf1cc9ff
Michael <okoronumichael9224@gmail.com>
2022-09-15 10:58:41 -0700

    Task_10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle
+ * @size: length of sides
+ *
+ * Return: void
+*/
+
+void print_triangle(int size)
+{
+	int line = 0;
+	int hash_tag;
+	int spaces;
+
+	if (size > 0)
+	{
+		while (line < size)
+		{
+			for (spaces = size - 1; spaces > line; spaces--)
+			{
+				putchar(' ');
+			}
+			for (hash_tag = 0; hash_tag < line + 1; hash_tag++)
+			{
+				putchar('#');
+				line++;
+			}
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
