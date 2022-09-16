6aa142931c481a204c3a20a2243bcbfcfff59dc2
Michael <okoronumichael9224@gmail.com>
2022-09-16 15:46:35 -0700

    Task_9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,22 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n < 101; n++)
+	for (n = 1; n <= 100; ++n)
 	{
-		if (n % 5 == 0 && n % 3 == 0)
-			putchar("FizzBuzz ");
-		else if (n % 5 == 0)
-			if (n == 100)
-				putchar("Buzz");
-			else
-				putchar("Buzz ");
-		else if (n % 3 == 0)
-			putchar("Fizz");
+		if (n % 3 == 0 && !(n % 5 == 0))
+			printf("Fizz");
+		else if (n % 5 == 0 && !(n % 3 == 0))
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
 		else
-			putchar("%d", n);
+			printf("%d", n);
+		
+		if (n != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	putchar("\n");
 
 	return (0);
 }
