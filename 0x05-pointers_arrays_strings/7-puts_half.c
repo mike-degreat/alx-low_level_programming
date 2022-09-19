090e53649da4214d425c0c06cbed9a1a001f39e4
Michael <okoronumichael9224@gmail.com>
2022-09-19 10:03:41 -0700

    Task_7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: 0
+*/
+
+void puts_half(char *str)
+{
+	int i, j, max;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	max = i;
+	j = max / 2;
+	while (j <= max)
+	{
+		putchar(str[j]);
+		j++;
+	}
+	putchar('\n');
+}
