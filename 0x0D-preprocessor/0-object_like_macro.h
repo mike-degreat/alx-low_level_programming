d41229c90fd958334548d1737f37d22ae990f3a7
Michael <okoronumichael9224@gmail.com>
2022-10-11 15:58:36 -0700

    macro fix

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_0BJECT_LIKE_MACRO_H_
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _0BJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /*_0_OBJECT_LIKE_MACRO_H_*/
+#endif /*_OBJECT_LIKE_MACRO_H_*/
